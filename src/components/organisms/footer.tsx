0c135d40bea99d3b76ddf0689e5f5f11397107f4
VaSTaR <vasilystarovoitov@gmail.com>
2023-12-27 22:28:40 +0300

    Footer responsive

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -6,13 +6,16 @@ import {Separator} from "@/components/ui/separator"
 const Footer = () => {
     return (
         <div
-            className={"h-32 py-4 bg-black text-gray-400 opacity-80 grid grid-cols-12 grid-rows-2"}>
+            className={"sm:h-32 p-4 sm:pt-6 md:pt-4 sm:pb-0 bg-black text-gray-400 opacity-80 sm:grid sm:grid-cols-12 sm:grid-rows-2"}>
             <div
-                className={"col-start-2 col-end-9 justify-self-start self-center p-1 flex"}>
-                <MapPin color={"gray"} className={"self-center"} />
-                <Separator orientation="vertical"
-                           className={"h-9 mx-2 self-center"} />
-                <div>
+                className={"sm:col-start-2 sm:col-end-9 sm:justify-self-start sm:self-center p-1 flex"}>
+                <div className={"w-11 flex self-center"}>
+                    <MapPin color={"gray"} size={"30px"}
+                            className={"sm:self-center"} />
+                    <Separator orientation="vertical"
+                               className={"h-9 mx-1 sm:mx-2 sm:self-center"} />
+                </div>
+                <div className={"self-center"}>
                     <p className={"text-xs"}>АДРЕС</p>
                     <p className={"text-sm"}>Московская область, Дмитровский
                         городской округ, рабочий посёлок Некрасовский</p>
@@ -20,18 +23,21 @@ const Footer = () => {
             </div>
 
             <div
-                className={"col-start-9 col-end-12 justify-self-end self-center p-1 flex"}>
-                <Clock3 color={"gray"} className={"self-center"} />
-                <Separator orientation="vertical"
-                           className={"h-8 mx-2 self-center"} />
-                <div>
+                className={"sm:col-start-9 sm:col-end-12 sm:justify-self-end sm:self-center p-1 flex"}>
+                <div className={"w-11 flex self-center"}>
+                    <Clock3 color={"gray"} size={"30px"}
+                            className={"sm:self-center"} />
+                    <Separator orientation="vertical"
+                               className={"h-9 mx-1 sm:mx-2 sm:self-center"} />
+                </div>
+                <div className={"self-center"}>
                     <p className={"text-xs"}>ХРАМ ОТКРЫТ</p>
                     <p className={"text-xs"}>ежедневно с 8.00 до 18.00</p>
                 </div>
             </div>
 
             <div
-                className={"h-3 col-span-12 justify-self-center self-center"}>
+                className={"sm:h-3 sm:col-span-12 sm:justify-self-center sm:self-center text-center"}>
                 <p className={"text-xs"}>© 2022-2024 Спиридоньевский храм</p>
             </div>
         </div>
