faf4adf12cf1f95afcbfc14a751c4a4b6e9afcb7
VaSTaR <vasilystarovoitov@gmail.com>
2023-12-30 11:53:20 +0300

    Убрал Layout у 404. Не работает localstorage на SSR

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import {HeadFC, Link, PageProps} from "gatsby"
-import Layout from "@/components/templates/layout";
 
 const pageStyles = {
     color: "#232129",
@@ -26,25 +25,23 @@ const codeStyles = {
 
 const NotFoundPage: React.FC<PageProps> = () => {
     return (
-        <Layout>
-            <div style={pageStyles}>
-                <h1 style={headingStyles}>Page not found</h1>
-                <p style={paragraphStyles}>
-                    Sorry 😔, we couldn’t find what you were looking for.
-                    <br />
-                    {process.env.NODE_ENV === "development" ? (
-                        <>
-                            <br />
-                            Try creating a page in <code
-                            style={codeStyles}>src/pages/</code>.
-                            <br />
-                        </>
-                    ) : null}
-                    <br />
-                    <Link to="/">Go home</Link>.
-                </p>
-            </div>
-        </Layout>
+        <div style={pageStyles}>
+            <h1 style={headingStyles}>Page not found</h1>
+            <p style={paragraphStyles}>
+                Sorry 😔, we couldn’t find what you were looking for.
+                <br />
+                {process.env.NODE_ENV === "development" ? (
+                    <>
+                        <br />
+                        Try creating a page in <code
+                        style={codeStyles}>src/pages/</code>.
+                        <br />
+                    </>
+                ) : null}
+                <br />
+                <Link to="/">Go home</Link>.
+            </p>
+        </div>
     )
 }
 
